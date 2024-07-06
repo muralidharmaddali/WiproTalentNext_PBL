fbb106afa6a1963b5ad060fa069fa6200a846bbf
muralidharmaddali <143109142+muralidharmaddali@users.noreply.github.com>
2024-07-06 10:09:20 +0530

    Update IsEven.java

--- Wipro Programs/Day 1/IsEven.java
+++ Wipro Programs/Day 1/IsEven.java
@@ -1,1 +1,11 @@
+import java.util.*;
 
+class usermain{
+  public class IsEven(int input1)
+  {
+        if(input1%2==0 ^ input1==0)
+            return 2;
+        else
+            return 1;
+}
+            
