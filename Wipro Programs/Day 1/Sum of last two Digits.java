aac8c732a0a58910069743d99fbdfa07bb5e0761
muralidharmaddali <143109142+muralidharmaddali@users.noreply.github.com>
2024-07-12 22:49:58 +0530

    Sum of last two Digits of given number

---
+++ Wipro Programs/Day 1/Sum of last two Digits.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import  java.util.*;
+
+class UserMainCode
+  {
+      public int addLastDigits(int input1,int input2)
+    {
+      input1 = Math.abs(input1);
+      input2 = Math.absinput2);
+      return input1%10 + input2%10;
+    }
+  }
