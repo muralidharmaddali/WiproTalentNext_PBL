6ee5fa77216149d219587d7b0186c41424a9c8f9
muralidharmaddali <143109142+muralidharmaddali@users.noreply.github.com>
2024-07-12 21:48:34 +0530

    Create isOdd.java

---
+++ Wipro Programs/Day 1/isOdd.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+  {
+    public int isOdd(int input1)
+    {
+        if(input1%2!=0)
+                {
+                    return 2;
+                }
+                return 1;
+    }
+}
